915a187808195ef7eed8d8f2dc6b69c2bd970606
Denis Simanchuk <sidenser@gmail.com>
2019-11-05 12:32:32 +0200

    added user, report, vendor

---
+++ src/Crowdin/Model/Vendor.php
@@ -0,0 +1,104 @@
+<?php
+
+
+namespace Crowdin\Model;
+
+/**
+ * Class Vendor
+ * @package Crowdin\Model
+ */
+class Vendor extends BaseModel
+{
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * @var string
+     */
+    protected $status;
+
+    public function __construct(array $data = [])
+    {
+        parent::__construct($data);
+        $this->id = (integer)$this->getDataProperty('id');
+        $this->name = (string)$this->getDataProperty('name');
+        $this->description = (string)$this->getDataProperty('description');
+        $this->status = (string)$this->getDataProperty('status');
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     */
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+}
