53c03a704a63848bf855c76b61de9ac5467d634c
Denis Simanchuk <sidenser@gmail.com>
2019-11-05 12:32:32 +0200

    added user, report, vendor

--- src/Crowdin/Api/ReportApi.php
+++ src/Crowdin/Api/ReportApi.php
@@ -1,5 +1,6 @@
 <?php
 
+
 namespace Crowdin\Api;
 
 use Crowdin\Model\DownloadFile;
@@ -16,7 +17,7 @@ class ReportApi extends AbstractApi
      * @param array $data
      * @return Report|null
      */
-    public function generate(int $projectId, array $data): ?Report
+    public function generate(int $projectId, array $data):?Report
     {
         $path = sprintf('projects/%s/reports', $projectId);
         return $this->_post($path, Report::class, $data);
@@ -27,7 +28,7 @@ class ReportApi extends AbstractApi
      * @param int $reportId
      * @return DownloadFile|null
      */
-    public function download(int $projectId, int $reportId): ?DownloadFile
+    public function download(int $projectId, int $reportId):?DownloadFile
     {
         $path = sprintf('projects/%d/reports/%d/download', $projectId, $reportId);
         return $this->_get($path, DownloadFile::class);
