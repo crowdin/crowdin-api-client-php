6b802b5b96cc2a5fe3030e237832459e3f32b183
Denis Simanchuk <sidenser@gmail.com>
2019-11-05 13:15:55 +0200

    cs fixed

--- src/Crowdin/Crowdin.php
+++ src/Crowdin/Crowdin.php
@@ -14,8 +14,6 @@ use Crowdin\Api\StorageApi;
 use Crowdin\Api\StringTranslationApi;
 use Crowdin\Api\StringTranslationApprovalApi;
 use Crowdin\Api\TaskApi;
-use Crowdin\Api\UserApi;
-use Crowdin\Api\VendorApi;
 use Crowdin\Api\VoteApi;
 use Crowdin\Http\Client\CrowdinHttpClientFactory;
 use Crowdin\Http\Client\CrowdinHttpClientInterface;
