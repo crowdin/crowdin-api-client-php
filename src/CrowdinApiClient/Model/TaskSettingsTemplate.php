b2e34a75dd7eb5c0b969fd74eefe73b6f286cb80
Thomas Chapuis <thomas.chapuis@wizbii.com>
2023-10-16 10:44:05 +0200

    feat(task-settings-template): Added Task Settings Template endpoints (#160)

---
+++ src/CrowdinApiClient/Model/TaskSettingsTemplate.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace CrowdinApiClient\Model;
+
+class TaskSettingsTemplate extends BaseModel
+{
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var array
+     */
+    protected $config;
+
+    /**
+     * @var string
+     */
+    protected $createdAt;
+
+    /**
+     * @var string
+     */
+    protected $updatedAt;
+
+    public function __construct(array $data = [])
+    {
+        parent::__construct($data);
+        $this->id = (integer)$this->getDataProperty('id');
+        $this->name = (string)$this->getDataProperty('name');
+        $this->config = (array)$this->getDataProperty('config');
+        $this->createdAt = (string)$this->getDataProperty('createdAt');
+        $this->updatedAt = (string)$this->getDataProperty('updatedAt');
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return array
+     */
+    public function getConfig(): array
+    {
+        return $this->config;
+    }
+
+    /**
+     * @param array $config
+     */
+    public function setConfig(array $config): void
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreatedAt(): string
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUpdatedAt(): string
+    {
+        return $this->updatedAt;
+    }
+}
---
+++ tests/CrowdinApiClient/Model/TaskSettingsTemplateTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace CrowdinApiClient\Model;
+
+use PHPUnit\Framework\TestCase;
+
+class TaskSettingsTemplateTest extends TestCase
+{
+    /**
+     * @var TaskSettingsTemplate
+     */
+    public $taskSettingsTemplate;
+
+    public $data = [
+        "id" => 1,
+        "name" => "Default template",
+        "config" => [
+            "languages" => [
+                [
+                    "languageId" => "uk",
+                    "userIds" => [
+                        1
+                    ]
+                ]
+            ]
+        ],
+        'createdAt' => '2019-09-23T09:35:31+00:00',
+        'updatedAt' => '2020-09-23T09:35:31+00:00',
+    ];
+
+    public function testLoadData()
+    {
+        $this->taskSettingsTemplate = new TaskSettingsTemplate($this->data);
+        $this->checkData();
+    }
+
+    public function testSetData()
+    {
+        $this->taskSettingsTemplate = new TaskSettingsTemplate();
+        $this->taskSettingsTemplate->setName($this->data['name']);
+        $this->taskSettingsTemplate->setConfig($this->data['config']);
+
+        $this->assertEquals($this->data['name'], $this->taskSettingsTemplate->getName());
+        $this->assertEquals($this->data['config'], $this->taskSettingsTemplate->getConfig());
+    }
+
+    public function checkData()
+    {
+        $this->assertEquals($this->data['id'], $this->taskSettingsTemplate->getId());
+        $this->assertEquals($this->data['config'], $this->taskSettingsTemplate->getConfig());
+        $this->assertEquals($this->data['createdAt'], $this->taskSettingsTemplate->getCreatedAt());
+        $this->assertEquals($this->data['updatedAt'], $this->taskSettingsTemplate->getUpdatedAt());
+    }
+
+}
