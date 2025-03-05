ac96d5a3e419bf11ab09b1e93dfe04c4a80004ec
Bohdan Bobko <172895427+bhdnb@users.noreply.github.com>
2025-03-05 17:28:08 +0200

    feat: Distribution API update (#222)

--- src/CrowdinApiClient/Api/DistributionApi.php
+++ src/CrowdinApiClient/Api/DistributionApi.php
@@ -37,10 +37,6 @@ class DistributionApi extends AbstractApi
      * Get Distribution
      * @link https://developer.crowdin.com/api/v2/#operation/api.projects.distributions.get API Documentation
      * @link https://developer.crowdin.com/enterprise/api/v2/#operation/api.projects.distributions.get API Documentation Enterprise
-     *
-     * @param int $projectId
-     * @param string $hash
-     * @return Distribution|null
      */
     public function get(int $projectId, string $hash): ?Distribution
     {
@@ -73,10 +69,6 @@ class DistributionApi extends AbstractApi
      * Edit Distribution
      * @link https://developer.crowdin.com/api/v2/#operation/api.projects.distributions.post API Documentation
      * @link https://developer.crowdin.com/enterprise/api/v2/#operation/api.projects.distributions.patch API Documentation Enterprise
-     *
-     * @param int $projectId
-     * @param Distribution $distribution
-     * @return Distribution|null
      */
     public function update(int $projectId, Distribution $distribution): ?Distribution
     {
@@ -88,11 +80,8 @@ class DistributionApi extends AbstractApi
      * Delete Distribution
      * @link https://developer.crowdin.com/api/v2/#operation/api.projects.distributions.delete API Documentation
      * @link https://developer.crowdin.com/enterprise/api/v2/#operation/api.projects.distributions.delete API Documentation Enterprise
-     *
-     * @param int $projectId
-     * @param string $hash
      */
-    public function delete(int $projectId, string $hash)
+    public function delete(int $projectId, string $hash): void
     {
         $path = sprintf('projects/%d/distributions/%s', $projectId, $hash);
         $this->_delete($path);
@@ -102,15 +91,10 @@ class DistributionApi extends AbstractApi
      * Release Distribution
      * @link https://developer.crowdin.com/api/v2/#operation/api.projects.distributions.release.post API Documentation
      * @link https://developer.crowdin.com/enterprise/api/v2/#operation/api.projects.distributions.release.post API Documentation Enterprise
-     *
-     * @param int $projectId
-     * @param string $hash
-     * @return DistributionRelease|null
      */
     public function release(int $projectId, string $hash): ?DistributionRelease
     {
         $path = sprintf('projects/%d/distributions/%s/release', $projectId, $hash);
-
         return $this->_create($path, DistributionRelease::class, []);
     }
 
@@ -118,15 +102,10 @@ class DistributionApi extends AbstractApi
      * Get Distribution Release
      * @link https://developer.crowdin.com/api/v2/#operation/api.projects.distributions.release.get API Documentation
      * @link https://developer.crowdin.com/enterprise/api/v2/#operation/api.projects.distributions.release.post API Documentation Enterprise
-     *
-     * @param int $projectId
-     * @param string $hash
-     * @return DistributionRelease|null
      */
     public function getRelease(int $projectId, string $hash): ?DistributionRelease
     {
         $path = sprintf('projects/%d/distributions/%s/release', $projectId, $hash);
-
         return $this->_get($path, DistributionRelease::class);
     }
 }
--- tests/CrowdinApiClient/Api/DistributionApiTest.php
+++ tests/CrowdinApiClient/Api/DistributionApiTest.php
@@ -10,170 +10,164 @@ use CrowdinApiClient\ModelCollection;
 
 class DistributionApiTest extends AbstractTestApi
 {
-    public function testList()
+    public function testList(): void
     {
         $this->mockRequest([
             'path' => '/projects/2/distributions',
             'method' => 'get',
-            'response' => '{
-                  "data": [
-                    {
-                      "data": {
-                        "hash": "e-4326c06be14321dd967b161a",
-                        "exportMode": "bundle",
-                        "name": "Test Distribution",
-                        "fileIds": [
-                          1
+            'response' => json_encode([
+                'data' => [
+                    [
+                        'data' => [
+                            'hash' => 'e-4326c06be14321dd967b161a',
+                            'exportMode' => 'bundle',
+                            'name' => 'Test Distribution',
+                            'fileIds' => [1],
+                            'bundleIds' => [1],
+                            'createdAt' => '2019-09-23T09:04:29+00:00',
+                            'updatedAt' => '2019-09-23T09:04:29+00:00',
                         ],
-                        "bundleIds": [
-                          1
-                        ],
-                        "createdAt": "2019-09-23T09:04:29+00:00",
-                        "updatedAt": "2019-09-23T09:04:29+00:00"
-                      }
-                    }
-                  ],
-                  "pagination": [
-                    {
-                      "offset": 0,
-                      "limit": 0
-                    }
-                  ]
-                }'
+                    ],
+                ],
+                'pagination' => [
+                    [
+                        'offset' => 0,
+                        'limit' => 0,
+                    ],
+                ],
+            ]),
         ]);
 
         $distributions = $this->crowdin->distribution->list(2);
+
         $this->assertInstanceOf(ModelCollection::class, $distributions);
         $this->assertCount(1, $distributions);
         $this->assertInstanceOf(Distribution::class, $distributions[0]);
         $this->assertEquals('e-4326c06be14321dd967b161a', $distributions[0]->getHash());
     }
 
-    public function testCreate()
+    public function testCreate(): void
     {
         $params = [
             'name' => 'Test Distribution',
-            'fileIds' =>
-                [
-                    0 => 0,
-                ],
+            'fileIds' => [0],
         ];
 
         $this->mockRequest([
             'path' => '/projects/2/distributions',
             'method' => 'post',
             'body' => json_encode($params),
-            'response' => '{
-              "data": {
-                "hash": "e-4326c06be14321dd967b161a",
-                "name": "Test Distribution",
-                "exportMode": "bundle",
-                "fileIds": [
-                  1
+            'response' => json_encode([
+                'data' => [
+                    'hash' => 'e-4326c06be14321dd967b161a',
+                    'name' => 'Test Distribution',
+                    'exportMode' => 'bundle',
+                    'fileIds' => [1],
+                    'bundleIds' => [1],
+                    'createdAt' => '2019-09-23T09:04:29+00:00',
+                    'updatedAt' => '2019-09-23T09:04:29+00:00',
                 ],
-                "bundleIds": [
-                  1
-                ],
-                "createdAt": "2019-09-23T09:04:29+00:00",
-                "updatedAt": "2019-09-23T09:04:29+00:00"
-              }
-            }'
-
+            ]),
         ]);
 
         $distribution = $this->crowdin->distribution->create(2, $params);
+
         $this->assertInstanceOf(Distribution::class, $distribution);
         $this->assertEquals('Test Distribution', $distribution->getName());
         $this->assertEquals("e-4326c06be14321dd967b161a", $distribution->getHash());
     }
 
-    public function testGetAndUpdate()
+    public function testGetAndUpdate(): void
     {
-        $this->mockRequestGet('/projects/2/distributions/e-4326c06be14321dd967b161a', '{
-              "data": {
-                "hash": "e-4326c06be14321dd967b161a",
-                "name": "Test Distribution",
-                "exportMode": "bundle",
-                "fileIds": [
-                  1
+        $this->mockRequestGet(
+            '/projects/2/distributions/e-4326c06be14321dd967b161a',
+            json_encode([
+                'data' => [
+                    'hash' => 'e-4326c06be14321dd967b161a',
+                    'name' => 'Test Distribution',
+                    'exportMode' => 'bundle',
+                    'fileIds' => [1],
+                    'bundleIds' => [1],
+                    'createdAt' => '2019-09-23T09:04:29+00:00',
+                    'updatedAt' => '2019-09-23T09:04:29+00:00',
                 ],
-                "bundleIds": [
-                  1
-                ],
-                "createdAt": "2019-09-23T09:04:29+00:00",
-                "updatedAt": "2019-09-23T09:04:29+00:00"
-              }
-          }');
+            ])
+        );
 
         $distribution = $this->crowdin->distribution->get(2, 'e-4326c06be14321dd967b161a');
+
         $this->assertInstanceOf(Distribution::class, $distribution);
         $this->assertEquals('e-4326c06be14321dd967b161a', $distribution->getHash());
 
-        $this->mockRequestPatch('/projects/2/distributions/e-4326c06be14321dd967b161a', '{
-              "data": {
-                "hash": "e-4326c06be14321dd967b161a",
-                "name": "test edit",
-                "exportMode": "bundle",
-                "fileIds": [
-                  1
+        $this->mockRequestPatch(
+            '/projects/2/distributions/e-4326c06be14321dd967b161a',
+            json_encode([
+                'data' => [
+                    'hash' => 'e-4326c06be14321dd967b161a',
+                    'name' => 'test edit',
+                    'exportMode' => 'bundle',
+                    'fileIds' => [1],
+                    'bundleIds' => [1],
+                    'createdAt' => '2019-09-23T09:04:29+00:00',
+                    'updatedAt' => '2019-09-23T09:04:29+00:00',
                 ],
-                "bundleIds": [
-                  1
-                ],
-                "createdAt": "2019-09-23T09:04:29+00:00",
-                "updatedAt": "2019-09-23T09:04:29+00:00"
-              }
-        }');
+            ])
+        );
 
         $distribution->setName('test edit');
         $distribution = $this->crowdin->distribution->update(2, $distribution);
+
         $this->assertInstanceOf(Distribution::class, $distribution);
         $this->assertEquals('e-4326c06be14321dd967b161a', $distribution->getHash());
         $this->assertEquals('test edit', $distribution->getName());
     }
 
-    public function testDelete()
+    public function testDelete(): void
     {
         $this->mockRequestDelete('/projects/2/distributions/e-4326c06be14321dd967b161a');
         $this->crowdin->distribution->delete(2, 'e-4326c06be14321dd967b161a');
     }
 
-    public function testRelease()
+    public function testRelease(): void
     {
         $this->mockRequest([
             'path' => '/projects/2/distributions/e-4326c06be14321dd967b161a/release',
             'method' => 'post',
-            'response' => '{
-              "data": {
-                "status": "success",
-                "progress": 0,
-                "currentLanguageId": 1,
-                "currentFileId": 1,
-                "date": "2019-09-23T09:04:29+00:00"
-              }
-            }'
-
+            'response' => json_encode([
+                'data' => [
+                    'status' => 'success',
+                    'progress' => 0,
+                    'currentLanguageId' => 1,
+                    'currentFileId' => 1,
+                    'date' => '2019-09-23T09:04:29+00:00',
+                ],
+            ]),
         ]);
 
         $distributionRelease = $this->crowdin->distribution->release(2, 'e-4326c06be14321dd967b161a');
+
         $this->assertInstanceOf(DistributionRelease::class, $distributionRelease);
         $this->assertEquals('success', $distributionRelease->getStatus());
         $this->assertEquals(1, $distributionRelease->getCurrentFileId());
     }
 
-    public function testGetRelease()
+    public function testGetRelease(): void
     {
-        $this->mockRequestGet('/projects/2/distributions/e-4326c06be14321dd967b161a/release', '{
-             "data": {
-                "status": "success",
-                "progress": 0,
-                "currentLanguageId": 1,
-                "currentFileId": 1,
-                "date": "2019-09-23T09:04:29+00:00"
-              }
-          }');
+        $this->mockRequestGet(
+            '/projects/2/distributions/e-4326c06be14321dd967b161a/release',
+            json_encode([
+                'data' => [
+                    'status' => 'success',
+                    'progress' => 0,
+                    'currentLanguageId' => 1,
+                    'currentFileId' => 1,
+                    'date' => '2019-09-23T09:04:29+00:00',
+                ],
+            ])
+        );
 
         $distributionRelease = $this->crowdin->distribution->getRelease(2, 'e-4326c06be14321dd967b161a');
+
         $this->assertInstanceOf(DistributionRelease::class, $distributionRelease);
         $this->assertEquals('success', $distributionRelease->getStatus());
         $this->assertEquals(1, $distributionRelease->getCurrentLanguageId());
